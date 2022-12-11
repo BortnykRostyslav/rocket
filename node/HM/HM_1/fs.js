86bc7ffb90c9bca43fa4b0897bce4b7ed5adc45c
Bortnyk Rostylav <bortnikrostislav370@gmail.com>
2022-12-12 01:28:46 +0200

    Сode optimization

--- node/HM/HM_1/fs.js
+++ node/HM/HM_1/fs.js
@@ -5,36 +5,23 @@ const path = require("path");
 const BoysPath = path.join(__dirname,'Boys')
 const GirlsPath = path.join(__dirname,'Girls')
 
-fs.readdir(BoysPath)
-    .then(Files =>{
-        for (const name of Files) {
-            const file = path.join(BoysPath,name)
-            fs.readFile(file)
-                .then(value => {
-                    const json = JSON.parse(value);
-                    if (json.gender === "female"){
-                        fs.rename(file, path.join(GirlsPath, name))
-                    } else{
-                        fs.rename(file, path.join(BoysPath, name))
-                    }
-                })
-        }
-    })
-fs.readdir(GirlsPath)
-    .then(Files =>{
-        for (const name of Files) {
-            const file = path.join(GirlsPath,name)
-            fs.readFile(file)
-                .then(value => {
-                    const json = JSON.parse(value);
-                    if (json.gender === "male"){
-                        fs.rename(file, path.join(BoysPath, name))
-                    } else{
-                        fs.rename(file, path.join(GirlsPath, name))
-                    }
-                })
-        }
-    })
+function sortGender (genderPath){
+    fs.readdir(genderPath)
+        .then(Files =>{
+            for (const name of Files) {
+                const file = path.join(genderPath,name)
+                fs.readFile(file)
+                    .then(value => {
+                        value = JSON.parse(value);
+                        value.gender === "female" ? fs.rename(file, path.join(GirlsPath, name)) : fs.rename(file, path.join(BoysPath, name));
+                    });
+            }
+        });
+}
+
+sortGender(BoysPath);
+sortGender(GirlsPath);
+
 
 
 
