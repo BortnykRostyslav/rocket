c9f861a46ebcdb57f07618ffe9b75b0e67091810
Bortnyk Rostylav <bortnikrostislav370@gmail.com>
2023-01-05 16:45:31 +0200

    HW_3 Full

--- node/booking/dataBase/User.js
+++ node/booking/dataBase/User.js
@@ -6,7 +6,7 @@ const UserSchema = new Schema({
         firstName: {type: String, trim: true, default: ' '},
         lastName: {type: String, trim: true, default:  ' '},
         email: {type: String, trim: true, lowercase: true, required: true, unique: true},
-        age: {type: Number},
+        age: {type: Number, min: 8, max: 101, require: true},
         password: {type: String, min: 8, require: true},
         role: { type: String, enum: Object.values(rolesEnum), default: rolesEnum.USER }
     },
