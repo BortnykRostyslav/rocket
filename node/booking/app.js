17a7fd30f2107ed57e8e9eaa01422b024daa3441
Bortnyk Rostylav <bortnikrostislav370@gmail.com>
2023-02-21 01:59:41 +0200

    Socket

--- node/booking/app.js
+++ node/booking/app.js
@@ -1,12 +1,13 @@
 require('module-alias/register');
 const express = require('express');
+const http = require('http');
 const mongoose = require('mongoose');
 const path = require('path');
+const socketIO = require('socket.io');
 const fileUpload = require('express-fileupload');
 const swaggerUi = require('swagger-ui-express');
 require('dotenv').config({path: path.join(__dirname, 'env', `.env.${process.env.NODE_ENV || 'local'}`)});
 global.rootPath = __dirname;
-
 
 const mainRouter = require('./api/api.router');
 const {PORT, MONGO_URL} = require('@configs/variables');
@@ -15,6 +16,40 @@ const {NotFound} = require('@error');
 const swaggerDocument = require('./swagger.json');
 const app = express();
 
+const server = http.createServer(app);
+
+const io = socketIO(server, { cors: { origin: '*' } });
+
+io.on('connection', (socket) => {
+    console.log(socket.id);
+    console.log(socket.handshake);
+
+    socket.on('send', (data) => {
+        console.log(data);
+
+        //SEND ON TO ONE
+        socket.emit('newMessage', { ok: true });
+    });
+
+    socket.on('getBroadcast', () => {
+        //SEND FOR ALL CONNECTED CLIENTS
+        // io.emit('forall', 'TEST');
+
+        //FOR ALL EXCEPT SENDER
+        socket.broadcast.emit('forall', 'I CANT SEE THIS');
+    });
+
+    socket.on('room:join', (roomData) => {
+        socket.join(roomData.roomId);
+
+        // SEND TO ROOM CLIENTS EXPECT SENDER
+        // socket.to(roomData.roomId).emit('room:newUser', socket.id);
+
+        // SEND TO ROOM ALL ROOM CLIENTS
+        io.to(roomData.roomId).emit('room:newUser', socket.id);
+    });
+
+});
 
 mongoose.set('debug', true);
 mongoose.set('strictQuery', true);
@@ -39,7 +74,7 @@ app.use('/api', mainRouter);
 app.use('*', _notFoundError);
 app.use(_mainErrorHandler);
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log('Listen', PORT);
     // require('./cronJobs');
 });
